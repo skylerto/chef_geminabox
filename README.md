a3255263e6276be718098c9ff95bf729329ff0ed
skylerto <skylerclayne@gmail.com>
2018-03-20 10:41:20 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_geminabox
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef_geminabox
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
