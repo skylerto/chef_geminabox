690b3c93e867e6f630a80a5421f8ad086d02e209
skylerto <skylerclayne@gmail.com>
2018-03-20 11:55:48 -0400

    gem in a box installation on centos via systemd

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,33 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+gem_package 'geminabox'
+directory '/opt/geminabox' do
+  owner   'root'
+  group   'root'
+  action  :create
+end
+
+template '/opt/geminabox/config.ru' do
+  source  'config.ru.erb'
+  owner   'root'
+  group   'root'
+end
+
+systemd_unit 'geminabox.service' do
+  content <<-SERVICE
+  [Unit]
+  Description=geminabox service
+  Requires=network.target
+
+  [Service]
+  WorkingDirectory=/opt/geminabox
+  User=root
+  ExecStart=/bin/bash -lc "/opt/chef/embedded/bin/rackup --host 0.0.0.0"
+
+  [Install]
+  WantedBy=multi-user.target
+  SERVICE
+  action [:create, :enable, :start]
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,5 +1,3 @@
-# # encoding: utf-8
-
 # Inspec test for recipe chef_geminabox::default
 
 # The Inspec reference, with examples and extensive documentation, can be
@@ -15,4 +13,20 @@ end
 # This is an example test, replace it with your own test.
 describe port(80), :skip do
   it { should_not be_listening }
+end
+
+describe port(9292), :skip do
+  it { should be_listening }
+end
+
+describe service('geminabox') do
+  it { should be_enabled }
+end
+
+describe file('/opt/geminabox') do
+  it { should be_directory }
+end
+
+describe file('/opt/geminabox/config.ru') do
+  it { should be_file }
 end
